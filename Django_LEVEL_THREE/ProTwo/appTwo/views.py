cc3ccf46236712afbf12e24008ed3e38ff6fcb66
John Jestin S <johnjestin6511@gmail.com>
2025-10-02 20:53:12 +0530

    Complete SignUp Page with save data

---
+++ Django_LEVEL_THREE/ProTwo/appTwo/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from appTwo.forms import NewUserForm
+
+# Create your views here.
+def index(request):
+    return render(request,'appTwo/index.html')
+
+def user(request):
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+        else:
+            print("ERROR FOUND")
+    return render(request,'appTwo/user_page.html',{'form':form})
+    
