88aefd1aaab2ed400c893dc46a7e5af28a5adb5b
John Jestin S <johnjestin6511@gmail.com>
2025-10-02 15:51:10 +0530

    Completed First_app--> Form Validation

--- Django_LEVEL_THREE/firstproject/first_app/forms.py
+++ Django_LEVEL_THREE/firstproject/first_app/forms.py
@@ -1,6 +1,23 @@
 from django import forms
+from django.core import validators
+
+
 
 class Formname(forms.Form):
     Name = forms.CharField()
     Email = forms.EmailField()
+    Verfiy_Email = forms.EmailField(label="Enter your email again:")
     Text = forms.CharField(widget=forms.Textarea)
+
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data["Email"]
+        Vemail = all_clean_data["Verfiy_Email"]
+
+        if email != Vemail:
+            raise forms.ValidationError("Make sure Email Matchs!")
+    # def clean_botcatcher(self):
+    #     botcatcher = self.cleaned_data["botcatcher"]
+    #     if len(botcatcher)>0:
+    #         raise forms.ValidationError("I got you BOT!")
+    #     return botcatcher
--- Django_LEVEL_THREE/firstproject/first_app/views.py
+++ Django_LEVEL_THREE/firstproject/first_app/views.py
@@ -7,7 +7,7 @@ def index(request):
     return render(request,'first_app/index.html')
 
 def form_name_view(request):
-    Form = forms.Formname()
+    form = forms.Formname()
 
     if request.method=="POST":
         form = forms.Formname(request.POST)
@@ -17,4 +17,5 @@ def form_name_view(request):
             print(f"Name: {form.cleaned_data['Name']}")
             print(f"Email: {form.cleaned_data['Email']}")
             print(f"Text: {form.cleaned_data['Text']}")
-    return render(request,'first_app/form_page.html',{'Form':Form})
+
+    return render(request,'first_app/form_page.html',{'Form':form})
